2d404d210b81b927a1f73d7c1a713e63e843d341
liununu <liununu5@gmail.com>
2020-02-15 17:09:44 +0800

    refactor(fibonacci): merge if statements in Fibonacci

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -3,10 +3,7 @@ package cc.xpbootcamp.warmup.fibonacci;
 public class Fibonacci {
 
   public long calculate(int position) {
-    if (position == 1) {
-      return 1;
-    }
-    if (position == 2) {
+    if (position == 1 || position == 2) {
       return 1;
     }
     if (position == 3) {
