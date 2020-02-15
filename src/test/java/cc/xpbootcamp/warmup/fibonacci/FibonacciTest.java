5010572d0c90895f666368bdafa83be3529578be
liununu <liununu5@gmail.com>
2020-02-15 17:08:13 +0800

    refactor(fibonacci): extract instance of Fibonacci in test

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -5,11 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
 class FibonacciTest {
+  private Fibonacci fibonacci = new Fibonacci();
 
   @Test
   void should_return_1_when_calculate_given_position_is_1() {
-    Fibonacci fibonacci = new Fibonacci();
-
     long result = fibonacci.calculate(1);
 
     assertThat(result).isEqualTo(1);
@@ -17,8 +16,6 @@ class FibonacciTest {
 
   @Test
   void should_return_1_when_calculate_given_position_is_2() {
-    Fibonacci fibonacci = new Fibonacci();
-
     long result = fibonacci.calculate(2);
 
     assertThat(result).isEqualTo(1);
@@ -26,8 +23,6 @@ class FibonacciTest {
 
   @Test
   void should_return_2_when_calculate_given_position_is_3() {
-    Fibonacci fibonacci = new Fibonacci();
-
     long result = fibonacci.calculate(3);
 
     assertThat(result).isEqualTo(2);
