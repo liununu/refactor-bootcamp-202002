b791cdb3e28d1c2b3bef31f4155aff13521425d2
liununu <liununu5@gmail.com>
2020-02-15 17:02:25 +0800

    feat(fibonacci): impl calculate fibonacci when position is 1

---
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -0,0 +1,11 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+public class Fibonacci {
+
+  public long calculate(int position) {
+    if (position == 1) {
+      return 1;
+    }
+    return 0;
+  }
+}
---
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -0,0 +1,17 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class FibonacciTest {
+
+  @Test
+  void should_return_1_when_calculate_given_position_is_1() {
+    Fibonacci fibonacci = new Fibonacci();
+
+    long result = fibonacci.calculate(1);
+
+    assertThat(result).isEqualTo(1);
+  }
+}
