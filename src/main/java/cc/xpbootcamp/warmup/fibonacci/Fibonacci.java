dce6f2aafc542461ce23318eee68b1c90c6103be
liununu <liununu5@gmail.com>
2020-02-20 16:37:24 +0800

    refactor(fibonacci)(code review): use map to cache result

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -2,10 +2,14 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 import cc.xpbootcamp.warmup.fibonacci.exception.PositionOutOfBoundsException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Fibonacci {
 
   private static final int MIN_POSITION = 1;
   private static final int MAX_POSITION = 50;
+  private static final Map<Integer, Long> fibonacciResultCacheMap = new HashMap<>();
 
   public long calculate(int position) {
     if (isPositionOutOfBounds(position)) {
@@ -14,7 +18,8 @@ public class Fibonacci {
     if (isFirstOrSecondPosition(position)) {
       return 1;
     }
-    return calculate(position - 2) + calculate(position - 1);
+    return fibonacciResultCacheMap.computeIfAbsent(
+        position, key -> calculate(key - 1) + calculate(key - 2));
   }
 
   private boolean isPositionOutOfBounds(int position) {
